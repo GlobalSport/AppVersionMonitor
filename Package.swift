aeed413daea124cbd64bfd231a4945fc7b04448d
jean torres <j.torres@mycoachsport.com>
2021-12-22 15:53:50 +0100

    fix package

--- Package.swift
+++ Package.swift
@@ -10,13 +10,23 @@ let package = Package(
         ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "AppVersionMonitor",
+            targets: ["AppVersionMonitor"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-     ],
+    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "AppVersionMonitor",
+            dependencies: []),
+        
+        .testTarget(
+            name: "AppVersionMonitorTests",
+            dependencies: ["AppVersionMonitor"]),
     ]
 )
